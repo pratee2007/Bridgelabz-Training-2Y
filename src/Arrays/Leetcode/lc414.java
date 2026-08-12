59f6723ca8723bd18a9f65b6d26931aadac424ad
Prateek Kumar Singh <prateekkumarsingh159@gmail.com>
2026-08-13 00:44:36 +0530

    Revert "leetcode"
    This reverts commit 1a7d7f6757e2564836625f65d4faf592ab180130.

--- src/Arrays/Leetcode/lc414.java
+++ src/Arrays/Leetcode/lc414.java
@@ -1,22 +1,4 @@
 package Arrays.Leetcode;
-import java.util.Arrays;
-public class lc414 {
-    public static void main(String[] args) {
-        int[] arr={4,3,2,1};
-        System.out.println(thirdMax(arr));
-    }
-    static int thirdMax(int[] nums) {
-        Arrays.sort(nums);
-        int count = 1;
 
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i] != nums[i - 1]) {
-                count++;
-                if (count == 3) {
-                    return nums[i - 1];
-                }
-            }
-        }
-        return nums[nums.length - 1];
-    }
+public class lc414 {
 }
